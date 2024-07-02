947186cbd60080b41e1a27875a8e7b5047add9d9
Muneeb Ahmad <mahmad.itp@sparkcognition.com>
2024-07-02 08:49:20 +0500

    Adding Ingress manifest

--- cluster_bootstrap_script.sh
+++ cluster_bootstrap_script.sh
@@ -3,13 +3,10 @@
 usage() {
   echo "Usage: ./cluster_bootstrap_script.sh [options]"
   echo "Options:"
-  echo '  -i                Use to Install Kubectl, AWS CLI, Helm to interact with k8s API' >&2
-  echo '  -s                Use to deploy Ingress Controller, certificate Manager for expose and use TLS communication' >&2
-  echo '  -g                Use to deploy ARC Action Runner Controller' >&2
-  echo '  -c                Target Cluster Name e.g rak-prod-eksdemo' >&2
-  echo '  -l                Target Cluster Region Name' >&2
-  echo '  -a                Application Name e.g -a java-web-app. Use to deploy Java Web App in the cluster' >&2
-  echo '  -r                Assume Role ARN to get login into AWS and execute the script' >&2
+  echo '  -i                To Install Kubectl, AWS CLI, Helm to interact with k8s API' >&2
+  echo '  -s                To deploy Ingress Controller, certificate Manager for expose and use TLS communication' >&2
+  echo '  -g                To deploy ARC Action Runner Controller' >&2
+  echo '  -a                Application Name e.g -a java-web-app. To deploy Application in the cluster' >&2
   exit 1
 }
 
@@ -27,7 +24,7 @@ then
 fi
 
 # Read parameters required
-while getopts isga:c:r: OPTION
+while getopts isga:c:r:l: OPTION
 do
   case ${OPTION} in
     i)
@@ -43,56 +40,17 @@ do
     g)
       github_runner=true
       ;;
-    c)
-      EKS_CLUSTER_NAME=${OPTARG}
-      ;;
-    l)
-      AWS_REGION=${OPTARG}
-      ;;
-    r)
-      AWS_ROLE_ARN=${OPTARG}
-      ;;
     \?)  
       usage
       ;;
   esac
 done
 
-####### Configuring Authentication for AWS using Temporary Creds #########
-echo "Region: $AWS_REGION"
-echo "EKS Cluster: $EKS_CLUSTER_NAME"
-
-# Assume the IAM role and export the temporary credentials
-ROLE_ARN=AWS_ROLE_ARN #"arn:aws:iam::637423397994:role/GitHub_Actions_CICD_Role"
-SESSION_NAME="AssumeGitHubActionsRoleSession"
-
-TEMP_CREDENTIALS=$(aws sts assume-role --role-arn $ROLE_ARN --role-session-name $SESSION_NAME)
-
-export AWS_ACCESS_KEY_ID=$(echo $TEMP_CREDENTIALS | jq -r '.Credentials.AccessKeyId')
-export AWS_SECRET_ACCESS_KEY=$(echo $TEMP_CREDENTIALS | jq -r '.Credentials.SecretAccessKey')
-export AWS_SESSION_TOKEN=$(echo $TEMP_CREDENTIALS | jq -r '.Credentials.SessionToken')
-
-# Check if credentials are set correctly
-if [ -z "$AWS_ACCESS_KEY_ID" ] || [ -z "$AWS_SECRET_ACCESS_KEY" ] || [ -z "$AWS_SESSION_TOKEN" ]; then
-    echo "Failed to assume role and retrieve credentials."
-    exit 1
-fi
-
-# Verify the temporary credentials
-aws sts get-caller-identity
-
-if [ $? -ne 0 ]; then
-    echo "Cluster Authentication failed"
-    exit 1
-fi
-
-
-##### Installing Utlities in the cluster. ######
+##### Installing CLI Utlities in the cluster. ######
 if [[ ${utilities} = 'true' ]]
 then
   log "Installing AWS CLI Version: v2"
   sudo apt install unzip -y
-  sudo apt install jq -y
   curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
   unzip awscliv2.zip
   sudo ./aws/install
@@ -114,6 +72,40 @@ then
   then
     echo "Utilities are not Installted Correctly." >&2
     exit 1
+  fi
+fi
+
+####### Configuring Authentication for AWS using Temporary Creds #########
+if [[ ${Ingress_CertManger} = 'true' || ${github_runner} = 'true' || ${deploy_app} = 'true' ]]
+then
+  read -p "Please Provide AWS Region: " AWS_REGION
+  read -p "Please Provide Target Cluster Name: " EKS_CLUSTER_NAME
+  read -p "Please Provide Role ARN to Assume: " AWS_ROLE_ARN
+  echo "Region: $AWS_REGION"
+  echo "EKS Cluster: $EKS_CLUSTER_NAME"
+  sudo apt install jq -y
+  # Assume the IAM role and export the temporary credentials
+  ROLE_ARN=${AWS_ROLE_ARN} #"arn:aws:iam::637423397994:role/GitHub_Actions_CICD_Role"
+  SESSION_NAME="AssumeGitHubActionsRoleSession"
+
+  TEMP_CREDENTIALS=$(aws sts assume-role --role-arn $ROLE_ARN --role-session-name $SESSION_NAME)
+
+  export AWS_ACCESS_KEY_ID=$(echo $TEMP_CREDENTIALS | jq -r '.Credentials.AccessKeyId')
+  export AWS_SECRET_ACCESS_KEY=$(echo $TEMP_CREDENTIALS | jq -r '.Credentials.SecretAccessKey')
+  export AWS_SESSION_TOKEN=$(echo $TEMP_CREDENTIALS | jq -r '.Credentials.SessionToken')
+
+  # Check if credentials are set correctly
+  if [ -z "$AWS_ACCESS_KEY_ID" ] || [ -z "$AWS_SECRET_ACCESS_KEY" ] || [ -z "$AWS_SESSION_TOKEN" ]; then
+      echo "Failed to assume role and retrieve credentials."
+      exit 1
+  fi
+
+  # Verify the temporary credentials
+  aws sts get-caller-identity
+
+  if [ $? -ne 0 ]; then
+      echo "Cluster Authentication failed"
+      exit 1
   fi
 fi
 
@@ -149,6 +141,7 @@ fi
 ###### Installing Action Runnder Controller ######
 if [[ ${github_runner} = 'true' ]]
 then
+  
   # Log into the Cluster
   aws eks --region ${AWS_REGION} update-kubeconfig --name ${EKS_CLUSTER_NAME}
   if [[ "${?}" -ne 0 ]]
@@ -157,6 +150,7 @@ then
     exit 1
   fi
   #Creation Secrets in k8s cluster
+  kubectl create ns actions
   kubectl apply -f github-action-runner/runner-secret.yaml
 
   # Adding Helm Repo 
@@ -166,10 +160,10 @@ then
   helm install \
   actions-runner-controller actions-runner-controller/actions-runner-controller \
   --namespace actions \
-  --create-namespace \
   --version 0.22.0 \
   --set syncPeriod=1m
   
+  sleep 20
   # Runner Deployment 
   kubectl apply -f github-action-runner/runner-deployments.yaml
   kubectl apply -f github-action-runner/horizontal-scale-runner.yaml
